c481d47b15ed417f6082b6148b4460f5e0e94820
Your Name <you@example.com>
2021-01-03 19:30:36 +0530

    trying GUIX

---
+++ STM32_GUIX/guix/gx_display_driver.h
@@ -0,0 +1,22 @@
+/**************************************************************************/
+/*                                                                        */
+/*       Copyright (c) Microsoft Corporation. All rights reserved.        */
+/*                                                                        */
+/*       This software is licensed under the Microsoft Software License   */
+/*       Terms for Microsoft Azure RTOS. Full text of the license can be  */
+/*       found in the LICENSE file at https://aka.ms/AzureRTOS_EULA       */
+/*       and in the root directory of this software.                      */
+/*                                                                        */
+/**************************************************************************/
+
+
+/**************************************************************************/
+/**************************************************************************/
+/**                                                                       */
+/** GUIX Component                                                        */
+/**                                                                       */
+/**   STM32F7XG Screen Driver                                             */
+/**                                                                       */
+/**************************************************************************/
+
+#define LCD_SCREEN_HANDLE         0x12345679
