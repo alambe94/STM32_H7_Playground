4b77d68e0d6001619e4c36f8141457b1fbe2cf94
alambe94 <lambealsaad@gmail.com>
2021-01-22 15:09:46 +0530

    check uvc diff

--- STM32_USB_RNDIS_UVC/RNDIS_UVC/Class/VIDEO/Inc/usbd_video.h
+++ STM32_USB_RNDIS_UVC/RNDIS_UVC/Class/VIDEO/Inc/usbd_video.h
@@ -27,7 +27,6 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include  "usbd_ioreq.h"
-#include  "usbd_composite.h"
 
 //#define USBD_UVC_FORMAT_UNCOMPRESSED
 
@@ -54,7 +53,7 @@ extern "C" {
 
 /* bEndpointAddress in Endpoint Descriptor */
 #ifndef UVC_IN_EP
-#define UVC_IN_EP                                     COMPOSITE_UVC_IN_EP
+#define UVC_IN_EP                                     0x81U
 #endif /* VIDEO_IN_EP */
 
 /* These defines shall be updated in the usbd_conf.h file */
@@ -126,8 +125,8 @@ extern "C" {
 
 
 #define UVC_REQ_READ_MASK                             0x80U
-#define UVC_VC_IF_NUM                                 COMPOSITE_UVC_VC_IF_NUM
-#define UVC_VS_IF_NUM                                 COMPOSITE_UVC_VS_IF_NUM
+#define UVC_VC_IF_NUM                                 0x00U
+#define UVC_VS_IF_NUM                                 0x01U
 #define UVC_TOTAL_IF_NUM                              0x02U
 
 #ifdef USBD_UVC_FORMAT_UNCOMPRESSED
